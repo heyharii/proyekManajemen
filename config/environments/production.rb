e6a97a4caffc4e2c1fa54f0a50117df9669c7d94
hari <neogalaxy45@gmail.com>
2018-04-09 15:50:26 +0700

    updated application to utilize SendGrid in Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,10 +95,11 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { :host => 'proyekmanajemen1.herokuapp.com' }
   ActionMailer::Base.smtp_settings = {
     :address          => "smtp.sendgrid.net",
-    :port             => "25",
+    :port             => "587",
     :authentication   => :plain,
     :user_name        => ENV['SENDGRID_USERNAME'],
     :password         => ENV['SENDGRID_PASSWORD'],
-    :domain           => ENV['SENDGRID_DOMAIN']
+    :domain           => ENV['SENDGRID_DOMAIN'],
+    :enable_starttls_auto => true
   }
 end
