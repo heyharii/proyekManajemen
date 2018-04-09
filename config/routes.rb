50e2c7d30a33e03b3b11ab4ccba574b24410c456
hari <neogalaxy45@gmail.com>
2018-04-09 14:05:54 +0700

    deploy heroku

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+
+  resources :projects
+  resources :teams
+	as :user do
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
+  end
+
+   devise_for :users, controllers: {
+  	registrations: 'registrations',
+  	confirmations: 'confirmations'
+  }
+  
+  root 'home#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
